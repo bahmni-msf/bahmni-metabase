a93d910969a83b160512c9a632ed02592795c79b
sindhura-kaparthi <sindhura.kaparthi@thoughtworks.com>
2022-12-07 14:50:18 +0530

    BAH-2601 | Add reports for openmrsdb

---
+++ docker/scripts/metabase/collection/create_metabase_collection.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Adding Collection to Metabase"
+
+collection_response=$(curl -L -w "%{http_code}" -X POST  \
+       -H "Content-type:application/json" \
+       -H "X-Metabase-Session:${MB_TOKEN}" \
+       http://${MB_HOST}:${MB_PORT}/api/collection \
+       -d '{
+       "name": "Bahmni Analytics",
+       "color": "#DDDDDD"
+       }'
+)
+STATUS=${collection_response: -3}
+if [ "$STATUS" == 200 ]
+then
+    COLLECTION_ID=$(jq -s -r '.[0].id' <<< ${collection_response})
+    echo "Bahmni Collection added to Metabase with id:"${COLLECTION_ID}
+else
+    echo 'Could not create the Bahmni Collection in Metabase.' ${STATUS}
+fi
