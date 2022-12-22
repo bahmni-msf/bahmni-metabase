f106b5d684151ba69ef2353f34887e12fb2f1a4d
sindhura-kaparthi <107908355+sindhura-kaparthi@users.noreply.github.com>
2022-12-22 17:34:18 +0530

    BAH-2665 | Create Helm chart to deploy Metabase to Dev Lite (#4)
    Co-authored-by: kusumikab <kusumika.barua@thoughtworks.com>

--- package/docker/scripts/metabase/metabase_init.sh
+++ package/docker/scripts/metabase/metabase_init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+MB_HOST="localhost"
+MB_PORT=3000
+
 echo "Waiting for Metabase to start"
 while (! curl -s -m 5 http://${MB_HOST}:${MB_PORT}/api/session/properties -o /dev/null); do sleep 5; done
 
