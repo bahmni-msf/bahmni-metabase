3bc93acf12212dc3f83b7df25f268276f6cf0ab5
Kushboo <kushboojain.r@thoughtworks.com>
2023-03-22 12:47:50 +0530

    BAH-2908 | Removing quotes from sql

--- package/docker/scripts/reports/sql/NoOfDiagnosis.sql
+++ package/docker/scripts/reports/sql/NoOfDiagnosis.sql
@@ -1,6 +1,6 @@
-SELECT "visit_diagnoses"."coded_diagnosis" AS "coded_diagnosis", count(*) AS "count"
-FROM "visit_diagnoses"
-WHERE ("visit_diagnoses"."date_created" >= (CAST(date_trunc('week', ((now() + (INTERVAL '-1 week')) + (INTERVAL '1 day'))) AS timestamp) + (INTERVAL '-1 day'))
-   AND "visit_diagnoses"."date_created" < (CAST(date_trunc('week', (now() + (INTERVAL '1 day'))) AS timestamp) + (INTERVAL '-1 day')))
-GROUP BY "visit_diagnoses"."coded_diagnosis"
-ORDER BY "visit_diagnoses"."coded_diagnosis"
+SELECT vd.coded_diagnosis AS "coded_diagnosis", count(*) AS "count"
+FROM visit_diagnoses vd
+WHERE (vd.date_created >= (CAST(date_trunc('week', ((now() + (INTERVAL '-1 week')) + (INTERVAL '1 day'))) AS timestamp) + (INTERVAL '-1 day'))
+    AND vd.date_created < (CAST(date_trunc('week', (now() + (INTERVAL '1 day'))) AS timestamp) + (INTERVAL '-1 day')))
+GROUP BY vd.coded_diagnosis
+ORDER BY vd.coded_diagnosis
