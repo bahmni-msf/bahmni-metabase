95aa1f307d7688f6fe384e685c966fcb1e00d9e4
srkrishna <radhakrishnan.sekar@thoughtworks.com>
2024-10-09 13:19:05 +0530

    Remove openmrs db connection

--- package/docker/scripts/database/add_databases.sh
+++ package/docker/scripts/database/add_databases.sh
@@ -7,14 +7,6 @@ get_db_data()
     cat <<EOF
     [
         {
-            "DB_HOST": "${OPENMRS_DB_HOST}",
-            "DB_NAME": "${OPENMRS_DB_NAME}",
-            "DB_USERNAME": "${OPENMRS_DB_USERNAME}",
-            "DB_PASSWORD": "${OPENMRS_DB_PASSWORD}",
-            "DB_TYPE": "mysql",
-            "DB_REF_NAME": "openmrs"
-        },
-        {
             "DB_HOST": "${MART_DB_HOST}",
             "DB_NAME": "${MART_DB_NAME}",
             "DB_USERNAME": "${MART_DB_USERNAME}",
@@ -55,10 +47,10 @@ add_DB_to_metabase(){
     then
         id=$(jq -s -r '.[0].id' <<< ${database_response})
         eval "${DB_REF_NAME}=$id"
-        echo "$2 Database added to Metabase" 
+        echo "$2 Database added to Metabase"
         DATABASE_CREATED=true
     else
-        echo "error occured while connecting DB : $2" 
+        echo "error occured while connecting DB : $2"
     fi
 }
 
@@ -70,16 +62,16 @@ for ((i=0; i < $length; i++))
 do
     DB_DATA=$(jq ".[$i]" <<< ${DB_CONNECTIONS})
 
-    DB_HOST_NAME=$(echo "${DB_DATA}" | jq '.DB_HOST' | tr -d '"') 
-    DB_NAME=$(echo "${DB_DATA}" | jq '.DB_NAME' | tr -d '"') 
-    DB_USERNAME=$(echo "${DB_DATA}" | jq '.DB_USERNAME' | tr -d '"') 
-    DB_PASSWORD=$(echo "${DB_DATA}" | jq '.DB_PASSWORD' | tr -d '"') 
+    DB_HOST_NAME=$(echo "${DB_DATA}" | jq '.DB_HOST' | tr -d '"')
+    DB_NAME=$(echo "${DB_DATA}" | jq '.DB_NAME' | tr -d '"')
+    DB_USERNAME=$(echo "${DB_DATA}" | jq '.DB_USERNAME' | tr -d '"')
+    DB_PASSWORD=$(echo "${DB_DATA}" | jq '.DB_PASSWORD' | tr -d '"')
     DB_TYPE=$(echo "${DB_DATA}" | jq '.DB_TYPE' | tr -d '"')
     DB_REF_NAME=$(echo "${DB_DATA}" | jq '.DB_REF_NAME' | tr -d '"')
 
     echo "connecting DB : $DB_REF_NAME"
 
-    add_DB_to_metabase  $DB_TYPE $DB_REF_NAME $DB_HOST_NAME $DB_NAME $DB_USERNAME $DB_PASSWORD 
+    add_DB_to_metabase  $DB_TYPE $DB_REF_NAME $DB_HOST_NAME $DB_NAME $DB_USERNAME $DB_PASSWORD
 done
 
 if [ $DATABASE_CREATED == true ]
